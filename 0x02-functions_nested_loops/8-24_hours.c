eb3e8ca2bb8d930b8e33d7991be41c3a5eb960e1
fitse4christ <tfistum@gmail.com>
2022-12-14 14:48:55 -0800

    8-24_hoursc

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for(a = 0; a <= 2; a++)
+	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
@@ -20,7 +20,7 @@ void jack_bauer(void)
 					{
 						_putchar(a + '0');
 						_putchar(b + '0');
-						_putcchar(58);
+						_putchar(58);
 						_putchar(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
