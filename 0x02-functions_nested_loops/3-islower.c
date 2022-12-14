223e6550703a56611b8bfd1e6f7ad427a8b5b5b4
fitse4christ <tfistum@gmail.com>
2022-12-14 12:17:54 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-islower.c -o 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _islower check for the lower character
- * &c: the character to be checked
+ *_islower - Checks for the lower character
+ *@c: the character to be checked
  * Return: 1 for lower character 0 for anything else
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }
