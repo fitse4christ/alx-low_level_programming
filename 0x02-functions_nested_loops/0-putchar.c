8e704c4d13e4a9cb15aca27af0226810b39b6b95
fitse4christ <tfistum@gmail.com>
2022-12-14 06:19:28 -0800

    0-putch

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * Return: Always 0
  */
 int main(void)
-
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -14,5 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,12 @@
-#include "main.h"                                                                                                                           
-#include <unistd.h>                                                                                                                                      
-
-/**                                                                                                                                                       * _putchar -writes the character c                                          
- *@c: the character to print
- * Return: On success 1                                                                                                                       
- */                                                                                                                                                      
-
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar  - write the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1
+ */
 int _putchar(char c)
 
 	return (write(1, &c, 1));
+
