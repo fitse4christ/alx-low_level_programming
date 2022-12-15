6858a018e7068ac5ee8d335f812b1e0e9d13a851
fitse4christ <tfistum@gmail.com>
2022-12-15 13:58:26 -0800

    0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /*
- * main - test function that printsif integer is positive or negitive
+ * main - test function that printsif integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_or_negitive(i);
-
+	positive_or_negative(i);
 	return (0);
 }
